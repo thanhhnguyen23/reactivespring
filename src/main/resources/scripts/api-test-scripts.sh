d0c10edf727a4c064c48746bd6076b7d737ec6f2
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2023-01-04 22:54:23 -0500

    add basic post request test

--- src/main/resources/scripts/api-test-scripts.sh
+++ src/main/resources/scripts/api-test-scripts.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-curl http://localhost:8080/room/v1/reservation/1234
+
+# GET request
+curl http://localhost:8080/room/v1/reservation/1234 &&
+#
+# POST request
+curl --location --request POST 'localhost:8080/room/v1/reservation/' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "roomNumber": 123,
+    "checkIn": "2023-01-01",
+    "checkOut": "2023-02-01",
+    "price": 150
+
+}' &&
+#
